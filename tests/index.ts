e5c279395e40c175c7ee25e49c02e84ffb26084b
tuhana <git@tuhana.me>
2023-02-15 18:08:31 +0300

    add tests

---
+++ tests/index.ts
@@ -0,0 +1,26 @@
+import { assertSnapshot } from 'std:testing/snapshot';
+
+Deno.test('createGitHookScript() tests', async (t) => {
+  const createGitHookScript = (await import('../src/index.ts')).createGitHookScript;
+
+  await assertSnapshot(t, createGitHookScript(['lint']))
+  await assertSnapshot(t, createGitHookScript([
+    "!echo 'Changed branch, running lint tasks...'",
+    "lint ; lint:fmt",
+    "!echo 'Tasks ran successfully.'"
+  ]))
+  await assertSnapshot(t, createGitHookScript(['lint ; fmt && test ; notify']))
+  await assertSnapshot(t, createGitHookScript(['test1 && test2 && test3']))
+})
+
+Deno.test('getGitHooks() tests', async (t) => {
+  const getGitHooks = (await import('../src/index.ts')).getGitHooks;
+
+  assertSnapshot(t, await getGitHooks('tests/deno.test.jsonc'))
+})
+
+Deno.test("createHooks() tests", async (t) => {
+  const createHooks = (await import('../src/index.ts')).createHooks;
+
+  assertSnapshot(t, await createHooks('tests/deno.test.jsonc'))
+})
