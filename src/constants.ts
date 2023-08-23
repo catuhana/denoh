962cd72401796eb4c9a73d3f36fe33aa7460f922
tuhana <git@tuhana.me>
2023-08-23 14:23:34 +0300

    add more git hooks, move enums to separate file, create ExitCodes enum

--- src/constants.ts
+++ src/constants.ts
@@ -20,13 +20,14 @@ export const HOOKS = [
   'post-receive',
   'post-update',
   'reference-transaction',
+  'push-to-checkout',
+  'pre-auto-gc',
+  'post-rewrite',
+  'sendemail-validate',
+  'fsmonitor-watchman',
+  'p4-changelist',
+  'p4-prepare-changelist',
+  'p4-post-changelist',
+  'p4-pre-submit',
+  'post-index-change',
 ] as const;
-
-/**
- * Operators object for Logical AND, OR and separator.
- */
-export const OPERATORS = {
-  AND: '&&',
-  OR: '||',
-  SEPARATOR: ';',
-} as const;
